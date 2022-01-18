49a3565922466de0737702e606db73069de433b5
martinkurzjr <kurz.martin921@gmail.com>
2022-01-18 19:29:26 +0100

    added gpio script

--- scripts/interact_wosolc.py
+++ scripts/interact_wosolc.py
@@ -1,23 +1,20 @@
 # interact with contract web3 only
 from web3 import Web3
-
-# rom solcx import compile_standard, install_solc
-# import json
 import os
 from dotenv import load_dotenv
 import requests
 
 load_dotenv()
 
+# Instanciate Provider
 w3 = Web3(
     Web3.HTTPProvider("https://ropsten.infura.io/v3/9cfee390074a4c3e9d08ea3c4938aa99")
 )
+# Test if Provider is connected
 connection_established = w3.isConnected()
 print(f"Connection is established {connection_established}.")
 
 
-chain_id = 3
-my_address = "0x47106e27177592632C9AB08E75EA201b7Cb37e24"
 private_key = os.getenv("PRIVATE_KEY")
 
 abi = [
@@ -45,14 +42,25 @@ abi = [
     },
 ]
 
+# Get ABI from etherscan API
 etherscan_token = os.getenv("ETHERSCAN_TOKEN")
-
 abi_url = f"https://api.etherscan.io/api?module=contract&action=getabi&address=0x4733c0262f381E7C6DD061Ab2863c3bF328c7Bb7&apikey={etherscan_token}"
 response = requests.get(abi_url)
 abi_json = response.json()
-print(abi_json)
+# print(abi_json)
 
+# Read from contract on Blockchain
 iot_light = w3.eth.contract("0x4733c0262f381E7C6DD061Ab2863c3bF328c7Bb7", abi=abi)
 # print(iot_light.abi)
 rgb = iot_light.functions.light().call()
 print(rgb)
+
+# logik for led
+if rgb[0] > 0:
+    print("red")
+elif rgb[1] > 0:
+    print("green")
+elif rgb[2] > 0:
+    print("blue")
+else:
+    print("black")
---
+++ scripts/pin_test.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+# declare pin layout
+GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BCM)
+
+mode = GPIO.getmode()
+print(mode)
+
+# setup channel
+chan_list = [79]
+# GPIO.setup(channel, GPIO.IN)
+# GPIO.setup(chan_list, GPIO.OUT)
+# initial value
+GPIO.setup(chan_list, GPIO.OUT, initial=GPIO.HIGH)
+print("setup channel 79 to out and high")
+
+time.sleep(30)
+print("cleaning up the channels")
+GPIO.cleanup()
